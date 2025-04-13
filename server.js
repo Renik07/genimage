3ff5270911552855ae2e4a3ed283c363915e2e15
Renat <restspread@gmail.com>
2025-04-13 21:06:43 +0300

    change server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+
+const app = express();
+const PORT = 3000;
 
 // Раздача статики из папки public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Пример: запуск сервера
-app.listen(3000, () => {
-  console.log('Server started on http://localhost:3000');
+// Обработка корневого маршрута
+app.get('/', (req, res) => {
+  res.send('Привет! Сервер работает.');
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
 });

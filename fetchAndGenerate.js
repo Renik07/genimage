1128c7a5d771b877e229947f7705cd2ef0f902d3
Renat <restspread@gmail.com>
2025-04-14 14:18:54 +0300

    remove filter fetchAndGenerate.js

--- fetchAndGenerate.js
+++ fetchAndGenerate.js
@@ -1,17 +1,15 @@
-const axios = require('axios');
-const generateImage = require('./generateImage');
-const slugify = require('slugify'); // если хочешь красивые имена файлов
+const axios = require("axios");
+const generateImage = require("./generateImage");
+const slugify = require("slugify"); // если хочешь красивые имена файлов
 
 async function main() {
-  const response = await axios.get('https://leon.ru/blog/api/top-events');
+  const response = await axios.get("https://leon.ru/blog/api/top-events");
   const events = response.data;
 
   for (const event of events) {
-    if (event.league_name === 'НБА') { // можно отфильтровать
-      const filename = slugify(event.slug) + '.png';
-      await generateImage(event, filename);
-      console.log('Сохранено изображение:', filename);
-    }
+    const filename = slugify(event.slug) + ".png";
+    await generateImage(event, filename);
+    console.log("Сохранено изображение:", filename);
   }
 }
 
